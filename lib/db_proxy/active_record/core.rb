e58272977f1e3604f90c7edc7584d53054737f60
michael.yang <michael.yang@edmodo.com>
2021-04-23 16:48:36 +0800

    prepended find

---
+++ lib/db_proxy/active_record/core.rb
@@ -0,0 +1,18 @@
+module DbProxy
+  module ActiveRecord
+    module Core
+      def self.prepended(base)
+        class << base
+          prepend ClassMethods
+        end
+      end
+
+      module ClassMethods
+        def find(*ids)
+          return all.find(ids.first) if ids.size == 1
+          super(*ids)
+        end
+      end
+    end
+  end
+end
