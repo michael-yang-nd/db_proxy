66aae7c40362ee3226202729ab60134e98e32709
michael.yang <michael.yang@edmodo.com>
2021-04-23 15:48:43 +0800

    first commit

---
+++ lib/db_proxy.rb
@@ -0,0 +1,10 @@
+require "db_proxy/version"
+
+module DbProxy
+  class Error < StandardError; end
+  # Your code goes here...
+
+  def self.hello
+    puts "hello from DbProxy"
+  end
+end
---
+++ lib/db_proxy/version.rb
@@ -0,0 +1,3 @@
+module DbProxy
+  VERSION = "0.1.0"
+end
